6f9e99042f1268a043595b6c1f9ae904612a61bb
pacimarco <m.paci1987@gmail.com>
2022-08-24 16:47:22 +0200

    v-for img main

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,111 @@
+<template>
+  
+      <header>
+          <img src="../assets/img/dc-logo.png" alt="logo">
+          <ul>
+              <li v-for="(link, index) in links" :key="index">
+                  <a :href="link.url" :class="link.active?'active': ''">{{link.text}}</a>
+              </li>
+              
+          </ul>
+      </header>
+  
+</template>
+
+<script>
+export default {
+name: 'MyHeader',
+data(){
+    return{
+        links:[
+            {
+                text: 'CHARACTERS',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'COMICS',
+                url: '#',
+                active: true
+            },
+            {
+                text: 'MOVIES',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'TV',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'GAMES',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'COLLECTIBLES',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'VIDEOS',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'FANS',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'NEWS',
+                url: '#',
+                active: false
+            },
+            {
+                text: 'SHOP',
+                url: '#',
+                active: false
+            },
+        ]
+    }
+}
+}
+</script>
+
+<style lang="scss">
+header{
+    text-align: center;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 20px;
+
+    img{
+        width: 60px;
+    }
+
+    ul{
+        list-style: none;
+        margin: 20px;
+
+        li{
+            display: inline-block;
+            margin: 0 10px;
+
+            a{
+                text-decoration: none;
+                font-size: 10px;
+                color: black;
+                padding: 10px;
+
+                &.active, &:hover{
+                    border-bottom: 2px solid blue;
+                }
+            }
+        }
+        
+    }
+}
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,62 @@
+<template>
+  <main>
+      <section class="section-1">
+          <h3>--> Content goes here --></h3>
+      </section>
+
+      <section class="section-2">
+          <div class="main-menu" v-for="(menu, index) in menu" :key="index">
+              <img :src="menu.src" alt="menu">
+              {{menu.text}}
+
+          </div>
+
+      </section>
+  </main>
+</template>
+
+<script>
+
+
+export default {
+name: 'MyMain',
+data(){
+    return{
+        menu:[
+            {
+                src: '../assets/img/buy-comics-digital-comics.png',
+                text: 'DIGITAL COMICS',
+            },
+            {
+                src: '../assets/img/buy-comics-merchandise.png',
+                text: 'DC MERCHANDISE',
+            },
+            {
+                src: '../assets/img/buy-comics-subscription.png',
+                text: 'SUBSCRIPTION',
+            },
+            {
+                src: '../assets/img/buy-comics-shop-locator.png',
+                text: 'COMICS SHOP LOCATOR',
+            },
+            {
+                src: '../assets/img/buy-dc-power-visa.svg',
+                text: 'DC POWER VISA',
+            }
+        ]
+    }
+}
+}
+</script>
+
+<style lang="scss">
+.section-1{
+    background-color: black;
+    height: 130px;
+    line-height: 130px;
+    color: white;
+    padding-left: 180px;
+}
+
+
+</style>
