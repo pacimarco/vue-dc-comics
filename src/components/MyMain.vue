d51aa4f296e3bf9688977738e08b3a717895b8d4
pacimarco <m.paci1987@gmail.com>
2022-08-25 16:11:32 +0200

    error img title in card v-for

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -2,6 +2,13 @@
   <main>
       <section class="section-1">
           <h3>--> Content goes here --></h3>
+          <div class="container comics-list">
+              <div v-for="(card, index) in comicsList" :key="index"   class="comics-card">
+                  <img :src="comicsList.thumb" alt="">
+              <h3 class="title-comics">{{comicsList.series}}</h3>
+              </div>
+              
+          </div>
       </section>
 
       <section class="section-2">
@@ -43,7 +50,81 @@ data(){
                 src: require('../assets/img/buy-dc-power-visa.svg'),
                 text: 'DC POWER VISA',
             }
-        ]
+        ],
+        comicsList:[
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+    "price": "$19.99",
+    "series": "Action Comics",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+    "price": "$3.99",
+    "series": "American Vampire 1976",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+    "price": "$16.99",
+    "series": "Aquaman",
+    "type": "graphic novel"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+    "price": "$2.99",
+    "series": "Batgirl",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+    "price": "$3.99",
+    "series": "Batman",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+    "price": "$2.99",
+    "series": "Batman Beyond",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+    "price": "$3.99",
+    "series": "Batman/Superman",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+    "price": "$4.99",
+    "series": "Batman/Superman Annual",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+    "price": "$5.99",
+    "series": "Batman: The Joker War Zone",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+    "price": "$6.99",
+    "series": "Batman: Three Jokers",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+    "price": "$4.99",
+    "series": "Batman: White Knight Presents: Harley Quinn",
+    "type": "comic book"
+  },
+  {
+    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+    "price": "$16.99",
+    "series": "Catwoman",
+    "type": "graphic novel"
+  }
+]
     }
 }
 }
@@ -52,10 +133,15 @@ data(){
 <style lang="scss">
 .section-1{
     background-color: black;
-    height: 130px;
+    height: 630px;
     line-height: 130px;
     color: white;
     padding-left: 180px;
+
+    .comics-list{
+        display: flex;
+        flex-wrap: wrap;
+    }
 }
 .section-2{
     background-color: rgb(12,105,247);
@@ -82,6 +168,13 @@ data(){
        
         
         
+    }
+}
+.comics-card{
+    flex-basis: 15%;
+
+    img{
+        max-width: 100%;
     }
 }
 
